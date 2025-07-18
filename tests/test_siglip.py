08afdcb8f7d242f1ae8c944ce7222dbe3cb5ab0f
pythoncrazy <pinakpaliwal@gmail.com>
2025-07-18 02:28:00 +0000

    test: added the final test to ensure that the siglip model works correctly (and it does): (1, 768) Max Image features absolute difference: 0.0013641715049743652 Max Text features absolute difference: 0.0021181106567382812 Max absolute difference: 0.0005598068237304688

--- tests/test_siglip.py
+++ tests/test_siglip.py
@@ -2,7 +2,7 @@ import jax.numpy as jnp
 import requests
 from flax import nnx
 from PIL import Image
-from transformers import SiglipProcessor, SiglipTextModel, SiglipVisionModel
+from transformers import AutoModel, SiglipProcessor, SiglipTextModel, SiglipVisionModel
 
 from jimm.models.siglip import SigLIP
 
@@ -33,7 +33,7 @@ def test_siglip_inference():
     image_features_jimm = nnx.jit(model.encode_image)(image_array)
 
     print(f"Max Image features absolute difference: {jnp.abs(image_features_jimm - image_features_ref).max()}")
-    # assert jnp.allclose(image_features_jimm, image_features_ref, atol=1e-2), f"Outputs don't match: {image_features_jimm} vs {image_features_ref}"
+    assert jnp.allclose(image_features_jimm, image_features_ref, atol=1e-2), f"Outputs don't match: {image_features_jimm} vs {image_features_ref}"
 
     # Test text encoder
     pytorch_text_model = SiglipTextModel.from_pretrained(HF_MODEL_NAME)
@@ -50,3 +50,20 @@ def test_siglip_inference():
 
     print(f"Max Text features absolute difference: {jnp.abs(text_features_jimm - text_features_ref).max()}")
     assert jnp.allclose(text_features_jimm, text_features_ref, atol=1e-2), f"Outputs don't match: {text_features_jimm} vs {text_features_ref}"
+
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+
+    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt")
+
+    pytorch_model = AutoModel.from_pretrained(HF_MODEL_NAME)
+    pytorch_model.eval()
+    outputs = pytorch_model(**inputs)
+    logits_per_image_ref = outputs.logits_per_image.detach().cpu().numpy()
+
+    model.eval()
+    image_array = jnp.transpose(inputs["pixel_values"].detach().cpu().numpy(), axes=(0, 2, 3, 1))
+    text_array = inputs["input_ids"].detach().cpu().numpy()
+    logits_per_image_flax = nnx.jit(model)(image_array, text_array)
+    print(f"Max absolute difference: {jnp.abs(logits_per_image_flax - logits_per_image_ref).max()}")
+    assert jnp.allclose(logits_per_image_flax, logits_per_image_ref, atol=1e-2), f"Outputs don't match: {logits_per_image_flax} vs {logits_per_image_ref}"
