fix: added gradient checkpointing for the vision transformer, and also switched to flax.nnx.rnglib.Rngs for typing

src/jimm/common/transformer.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 from flax import nnx
+from flax.nnx import rnglib
 from jax.sharding import Mesh
 from jax.sharding import PartitionSpec as P
 from jax.typing import DTypeLike
@@ -37,7 +38,7 @@ class TransformerEncoder(nnx.Module):
         use_gradient_checkpointing: bool = False,
         dtype: DTypeLike = jnp.float32,
         param_dtype: DTypeLike = jnp.float32,
-        rngs: nnx.Rngs = nnx.Rngs(0),
+        rngs: rnglib.Rngs = nnx.Rngs(0),
         mesh: Mesh | None = None,
     ) -> None:
         """Initialize a TransformerEncoder.
@@ -53,7 +54,7 @@ class TransformerEncoder(nnx.Module):
             use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
             dtype (DTypeLike): Data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): Data type for parameters. Defaults to jnp.float32.
-            rngs (nnx.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+            rngs (rnglib.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
             mesh (Mesh | None): JAX device mesh for parameter sharding. Defaults to None.
         """
         self.attn_mask = attn_mask
@@ -150,14 +151,14 @@ class Transformer(nnx.Module):
         mlp_dim: int,
         layers: int,
         num_heads: int,
-        layernorm_epsilon=1e-6,
+        layernorm_epsilon: float = 1e-6,
         dropout_rate: float = 0.0,
         attn_mask: Float[Array, "seq seq"] | None = None,
         use_quick_gelu: bool = False,
         use_gradient_checkpointing: bool = False,
         dtype: DTypeLike = jnp.float32,
         param_dtype: DTypeLike = jnp.float32,
-        rngs: nnx.Rngs = nnx.Rngs(0),
+        rngs: rnglib.Rngs = nnx.Rngs(0),
         mesh: Mesh | None = None,
     ):
         """Initialize a Transformer.
@@ -167,14 +168,15 @@ class Transformer(nnx.Module):
             mlp_dim (int): The dimension of the MLP layer.
             layers (int): The number of transformer layers.
             num_heads (int): The number of attention heads.
+            layernorm_epsilon (float): The epsilon used in layernorm calculation. Defaults to 1e-6.
             dropout_rate (float): The dropout rate. Defaults to 0.0.
-            attn_mask (Float[Array, "seq seq"]|None): Optional attention mask. Defaults to None.
+            attn_mask (Float[Array, "seq seq"] | None): Optional attention mask. Defaults to None.
             use_quick_gelu (bool): Whether to use quickgelu instead of gelu. Defaults to False.
             use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
             dtype (DTypeLike): The data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): The data type for parameters. Defaults to jnp.float32.
-            rngs (nnx.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
-            mesh (Mesh|None): JAX device mesh for parameter sharding. Defaults to None.
+            rngs (rnglib.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+            mesh (Mesh | None): JAX device mesh for parameter sharding. Defaults to None.
         """
         self.width = width
         self.layers = layers

src/jimm/common/vit.py
@@ -143,7 +143,7 @@ class VisionTransformerBase(nnx.Module):
             use_patch_bias (bool): Whether to use bias in the patch embedding convolution. Defaults to True.
             use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
             layernorm_epsilon (float): Epsilon for LayerNorm. Defaults to 1e-5.
-            rngs (nnx.Rngs): The random number generator state. Defaults to nnx.Rngs(0).
+            rngs (rnglib.Rngs): The random number generator state. Defaults to nnx.Rngs(0).
             dtype (DTypeLike): The data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): The data type for parameters. Defaults to jnp.float32.
             mesh (Mesh | None): The device mesh for parameter sharding.

src/jimm/models/siglip.py
@@ -2,6 +2,7 @@ from typing import Any, Set
 
 import jax.numpy as jnp
 from flax import nnx
+from flax.nnx import rnglib
 from jax.sharding import Mesh
 from jax.sharding import PartitionSpec as P
 from jaxtyping import Array, DTypeLike, Float, Int
@@ -24,7 +25,7 @@ class SigLIP(nnx.Module):
         transformer_heads: int,
         transformer_layers: int,
         use_gradient_checkpointing: bool = False,
-        rngs: nnx.Rngs = nnx.Rngs(0),
+        rngs: rnglib.Rngs = nnx.Rngs(0),
         dtype: DTypeLike = jnp.float32,
         param_dtype: DTypeLike = jnp.float32,
         mesh: Mesh | None = None,
@@ -43,10 +44,10 @@ class SigLIP(nnx.Module):
             transformer_heads (int): The number of attention heads in the transformer.
             transformer_layers (int): The number of layers in the transformer.
             use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
-            rngs (nnx.Rngs): The random number generator state. Defaults to nnx.Rngs(0).
+            rngs (rnglib.Rngs): The random number generator state. Defaults to nnx.Rngs(0).
             dtype (DTypeLike): The data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): The data type for parameters. Defaults to jnp.float32.
-            mesh (Mesh | None): The device mesh for parameter sharding. Defaults to None.
+            mesh (Mesh | None): Optional device mesh for parameter sharding. Defaults to None.
         """
         self.vision_layers = vision_layers
         self.vision_width = vision_width
@@ -177,17 +178,17 @@ class SigLIP(nnx.Module):
         return logits
 
     @classmethod
-    def from_pretrained(cls, model_name_or_path: str, use_pytorch: bool = False, mesh: Mesh | None = None, dtype: DTypeLike = jnp.float32, param_dtype: DTypeLike = jnp.float32, use_gradient_checkpointing: bool = False, rngs: nnx.Rngs = nnx.Rngs(0)) -> "SigLIP":
+    def from_pretrained(cls, model_name_or_path: str, use_pytorch: bool = False, mesh: Mesh | None = None, dtype: DTypeLike = jnp.float32, param_dtype: DTypeLike = jnp.float32, use_gradient_checkpointing: bool = False, rngs: rnglib.Rngs = nnx.Rngs(0)) -> "SigLIP":
         """Load a pretrained SigLIP model from a local path or HuggingFace Hub.
 
         Args:
             model_name_or_path (str): Path to local weights or HuggingFace model ID.
             use_pytorch (bool): Whether to load from PyTorch weights. Defaults to False.
-            mesh (Mesh|None): Optional device mesh for parameter sharding. Defaults to None.
+            mesh (Mesh | None): Optional device mesh for parameter sharding. Defaults to None.
             dtype (DTypeLike): Data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): Data type for parameters. Defaults to jnp.float32.
             use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
-            rngs (nnx.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+            rngs (rnglib.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
 
         Returns:
             SigLIP: Pretrained SigLIP model

src/jimm/models/vit.py
@@ -3,6 +3,7 @@ from typing import Any, Set
 
 import jax.numpy as jnp
 from flax import nnx
+from flax.nnx import rnglib
 from jax.sharding import Mesh
 from jax.sharding import PartitionSpec as P
 from jax.typing import DTypeLike
@@ -31,10 +32,11 @@ class VisionTransformer(nnx.Module):
         hidden_size: int = 768,
         dropout_rate: float = 0.1,
         use_quick_gelu: bool = False,
+        use_gradient_checkpointing: bool = False,
         do_classification: bool = True,
         dtype: DTypeLike = jnp.float32,
         param_dtype: DTypeLike = jnp.float32,
-        rngs: nnx.Rngs = nnx.Rngs(0),
+        rngs: rnglib.Rngs = nnx.Rngs(0),
         mesh: Mesh | None = None,
     ) -> None:
         """Initialize a Vision Transformer.
@@ -50,11 +52,12 @@ class VisionTransformer(nnx.Module):
             hidden_size (int): Size of the hidden dimension. Defaults to 768.
             dropout_rate (float): Dropout rate. Defaults to 0.1.
             use_quick_gelu (bool): Whether to use quickgelu instead of gelu. Defaults to False.
+            use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
             do_classification (bool): Whether to include the final classification head. Defaults to True.
             dtype (DTypeLike): Data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): Data type for parameters. Defaults to jnp.float32.
-            rngs (nnx.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
-            mesh (Mesh|None): Optional JAX device mesh for parameter sharding. Defaults to None.
+            rngs (rnglib.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+            mesh (Mesh | None): Optional JAX device mesh for parameter sharding. Defaults to None.
         """
         self.do_classification = do_classification
         self.encoder = VisionTransformerBase(
@@ -67,6 +70,7 @@ class VisionTransformer(nnx.Module):
             mlp_dim=mlp_dim,
             dropout_rate=dropout_rate,
             use_quick_gelu=use_quick_gelu,
+            use_gradient_checkpointing=use_gradient_checkpointing,
             use_pre_norm=False,
             use_patch_bias=True,
             layernorm_epsilon=1e-12,
@@ -102,7 +106,7 @@ class VisionTransformer(nnx.Module):
         return x
 
     @classmethod
-    def from_pretrained(cls, model_name_or_path: str, use_pytorch: bool = False, mesh: Mesh | None = None, dtype: DTypeLike = jnp.float32, param_dtype: DTypeLike = jnp.float32, rngs: nnx.Rngs = nnx.Rngs(0)) -> "VisionTransformer":
+    def from_pretrained(cls, model_name_or_path: str, use_pytorch: bool = False, mesh: Mesh | None = None, dtype: DTypeLike = jnp.float32, param_dtype: DTypeLike = jnp.float32, use_gradient_checkpointing: bool = False, rngs: rnglib.Rngs = nnx.Rngs(0)) -> "VisionTransformer":
         """Load a pretrained Vision Transformer from a local path or HuggingFace Hub.
 
         Args:
@@ -111,7 +115,9 @@ class VisionTransformer(nnx.Module):
             mesh (Mesh | None): Optional device mesh for parameter sharding. Defaults to None.
             dtype (DTypeLike): Data type for computations. Defaults to jnp.float32.
             param_dtype (DTypeLike): Data type for parameters. Defaults to jnp.float32.
-            rngs (nnx.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+            use_gradient_checkpointing (bool): Whether to use gradient checkpointing. Defaults to False.
+            rngs (rnglib.Rngs): Random number generator keys. Defaults to nnx.Rngs(0).
+
         Returns:
             VisionTransformer: Initialized Vision Transformer with pretrained weights
         """
@@ -181,6 +187,7 @@ class VisionTransformer(nnx.Module):
             dtype=dtype,
             param_dtype=dtype,
             rngs=rngs,
+            use_gradient_checkpointing=use_gradient_checkpointing,
         )
 
         flax_model_params_fstate = dict(nnx.to_flat_state(nnx.state(model, nnx.Param)))